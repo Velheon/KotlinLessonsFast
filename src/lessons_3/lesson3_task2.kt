c519e2e0ae7db53ee6e1d42b949fb754bc244d58
velheon <MaksiaNIC@yandex.ru>
2026-05-23 13:49:06 +0300

    KS-3-2 solved lesson_3 task_2

---
+++ src/lessons_3/lesson3_task2.kt
@@ -0,0 +1,15 @@
+package lessons_3
+
+fun main() {
+    var lastName = "Андреева "
+    val firstName = "Татьяна"
+    val surname = "Сергеевна"
+    var old = 20
+
+    println("$lastName $firstName $surname, $old")
+
+    lastName = "Сидорова"
+    old = 22
+
+    println("$lastName $firstName $surname, $old")
+}
