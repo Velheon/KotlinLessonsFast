e5005533f758d2dce71f204a9cb43530f807d6cf
velheon <MaksiaNIC@yandex.ru>
2026-05-25 01:32:16 +0300

    KS-3-4 solved lesson_3 task_4

---
+++ src/lessons_3/lesson3_task4.kt
@@ -0,0 +1,19 @@
+package lessons_3
+
+fun main() {
+    var from: String
+    var to: String
+    var moveNum: Int
+
+    from = "E2"
+    to = "E4"
+    moveNum = 1
+
+    println("[$from-$to, $moveNum]")
+
+    from = "D2"
+    to = "D3"
+    moveNum++
+
+    println("[$from-$to, $moveNum]")
+}
