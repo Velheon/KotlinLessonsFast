fb3cce83836bc079ce01e17eb193c6be45c8c444
velheon <MaksiaNIC@yandex.ru>
2026-05-25 13:06:07 +0300

    KS-3-4 solved lesson_3 task_4

--- src/lessons_3/lesson3_task4.kt
+++ src/lessons_3/lesson3_task4.kt
@@ -4,10 +4,10 @@ fun main() {
     var from = "E2"
     var to = "E4"
     var moveNum = 1
-    println("[$from-$to;$moveNum]")
+    println("$from-$to;$moveNum")
 
     from = "D2"
     to = "D3"
     moveNum++
-    println("[$from-$to;$moveNum]")
+    println("$from-$to;$moveNum")
 }
