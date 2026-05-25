2661d87d4d19692f40cd77a9fde2d8eecb251bd4
velheon <MaksiaNIC@yandex.ru>
2026-05-25 12:43:45 +0300

    KS-3-4 solved lesson_3 task_4

--- src/lessons_3/lesson3_task4.kt
+++ src/lessons_3/lesson3_task4.kt
@@ -1,19 +1,15 @@
 package lessons_3
 
 fun main() {
-    var from: String
-    var to: String
-    var moveNum: Int
+    var from: String = "E2"
+    var to: String = "E4"
+    var moveNum: Int = 1
 
-    from = "E2"
-    to = "E4"
-    moveNum = 1
-
-    println("[$from-$to, $moveNum]")
+    println("[$from-$to;$moveNum]")
 
     from = "D2"
     to = "D3"
     moveNum++
 
-    println("[$from-$to, $moveNum]")
+    println("[$from-$to;$moveNum]")
 }
