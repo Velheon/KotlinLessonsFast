aca6d54a310e494ad772017a67e7483e0b02d0ab
velheon <MaksiaNIC@yandex.ru>
2026-05-25 14:04:55 +0300

    KS-4-1 solved lesson_4 task_1

--- src/lessons_4/lesson4_task1.kt
+++ src/lessons_4/lesson4_task1.kt
@@ -5,11 +5,11 @@ fun main() {
     val reservedTableToday = 13
     val reservedTableTomorrow = 9
 
-    val isFreeToday: Boolean = reservedTableToday < TABLE_COUNT
-    val isFreeTomorrow: Boolean = reservedTableTomorrow < TABLE_COUNT
+    val isFreeToday = reservedTableToday < TABLE_COUNT
+    val isFreeTomorrow = reservedTableTomorrow < TABLE_COUNT
 
     println(
-        "Доступность столиков на сегодня: $isFreeToday\n" +
-        "Доступность столиков на завтра: $isFreeTomorrow"
+        "[Доступность столиков на сегодня: $isFreeToday\n]" +
+        "[Доступность столиков на завтра: $isFreeTomorrow]"
     )
 }
