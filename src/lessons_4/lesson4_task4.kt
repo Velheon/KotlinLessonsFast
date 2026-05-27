37e1931640679972ac8bdee5e3414bea5a676a20
velheon <MaksiaNIC@yandex.ru>
2026-05-27 22:55:11 +0300

    KS-4-4 solved lesson_4 task_4

---
+++ src/lessons_4/lesson4_task4.kt
@@ -0,0 +1,14 @@
+package lessons_4
+
+fun main() {
+    val day = 5
+    val isDayForArmsAndAbdominal = day % 2 == 1
+    val isDayForLegsAndBack = !isDayForArmsAndAbdominal
+
+    println(
+        "Упражнения для рук: $isDayForArmsAndAbdominal\n\n" +
+                "Упражнения для ног: $isDayForLegsAndBack\n\n" +
+                "Упражнения для спины: $isDayForLegsAndBack\n\n" +
+                "Упражнения для пресса: $isDayForArmsAndAbdominal"
+    )
+}
