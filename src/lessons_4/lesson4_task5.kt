a54769b561f3813087d00c875d4d77504e33790c
velheon <MaksiaNIC@yandex.ru>
2026-05-27 23:41:00 +0300

    KS-4-5 solved lesson_4 task_5

--- src/lessons_4/lesson4_task5.kt
+++ src/lessons_4/lesson4_task5.kt
@@ -4,12 +4,13 @@ const val MIN_CREW = 55
 const val MAX_CREW = 70
 const val ENOUGH_BOXES = 50
 fun main() {
-    val isUnbroken = false
-    val crew = 50
-    val box = 50
-    val isGoodWeather = true
-    val canDepart = (isUnbroken && MIN_CREW <= crew && crew <= MAX_CREW && box > ENOUGH_BOXES) ||
-            (!isUnbroken && crew == MAX_CREW && isGoodWeather && box >= ENOUGH_BOXES)
+    val isUnbroken = readln().toBoolean()
+    val crew = readln().toInt()
+    val boxes = readln().toInt()
+    val isGoodWeather = readln().toBoolean()
+    // В сценарии, где корабль целый, погодные условия не важны
+    val canDepart = (isUnbroken && MIN_CREW <= crew && crew <= MAX_CREW && boxes > ENOUGH_BOXES) ||
+            (!isUnbroken && crew == MAX_CREW && isGoodWeather && boxes >= ENOUGH_BOXES)
 
     println("Корабль может отправиться: $canDepart")
 }
