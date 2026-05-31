3101c71f507ad43dc49c5314a463c7bb9398a200
velheon <MaksiaNIC@yandex.ru>
2026-05-31 20:30:48 +0300

    KS-5-2 solved lesson_5 task_2

--- src/lessons_5/lesson5_task2.kt
+++ src/lessons_5/lesson5_task2.kt
@@ -1,12 +1,14 @@
 package lessons_5
 
 const val AGE_OF_MAJORITY = 18
-const val CURRENT_YEAR = 2026
+
 
 fun main() {
+    val currentYear = 2026
+
     println("Введите год вашего рождения:")
     val userBirthYear = readln().toInt()
-    val userAge = CURRENT_YEAR - userBirthYear
+    val userAge = currentYear - userBirthYear
 
     if (userAge >= AGE_OF_MAJORITY) {
         println("Показать экран со скрытым контентом")
