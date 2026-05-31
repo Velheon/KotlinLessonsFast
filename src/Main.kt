17dd2f8204246ff8c033bdd395abba209deed0f4
velheon <MaksiaNIC@yandex.ru>
2026-05-31 20:30:36 +0300

    KS-5-2 solved lesson_5 task_2

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,16 @@
 fun main() {
-    val userName = "Иван"
-    val letter = """
-    |Привет, $userName!
-    Ваш заказ принят.
-    |Спасибо за покупку.
-""".trimMargin()
+    val num = 6
+    val table = """
+        $num * 1 = ${(num * 1).toString().padStart(2, '0')}
+        $num * 2 = ${(num * 2).toString().padStart(2, '0')}
+        $num * 3 = ${(num * 3).toString().padStart(2, '0')}
+        $num * 4 = ${(num * 4).toString().padStart(2, '0')}
+        $num * 5 = ${(num * 5).toString().padStart(2, '0')}
+        $num * 6 = ${(num * 6).toString().padStart(2, '0')}
+        $num * 7 = ${(num * 7).toString().padStart(2, '0')}
+        $num * 8 = ${(num * 8).toString().padStart(2, '0')}
+        $num * 9 = ${(num * 9).toString().padStart(2, '0')}
+    """.trimIndent()
 
-    println(letter)
+    println(table)
 }
