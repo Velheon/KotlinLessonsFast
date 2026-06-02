79ea7c5f6b2a62eec78da300e777344d24227f38
velheon <MaksiaNIC@yandex.ru>
2026-06-03 00:37:52 +0300

    KS-6-3 solved lesson_6 task_3

---
+++ src/lessons_6/lesson6_task3.kt
@@ -0,0 +1,14 @@
+package lessons_6
+
+fun main() {
+    println("Введите время в секундах:")
+    val seconds = readln().toInt()
+    var counterSeconds = seconds
+
+    while (counterSeconds > 0) {
+        println("Осталось секунд: ${counterSeconds--}")
+        Thread.sleep(1000)
+    }
+
+    print("Время вышло")
+}
